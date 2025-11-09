1ba82a0f65c873811afcaef5ac0452b780f66c3c
PreaseP@H <dong123541@gmail.com>
2025-11-10 04:23:27 +0900

    플레이어 움직임 구현

--- player.py
+++ player.py
@@ -13,14 +13,11 @@ def space_down(e): # e is space down ?
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
 
-
 def right_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
 
-
 def left_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
-
 
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
@@ -51,7 +48,10 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
-
+# Player Move Action Speed
+TIME_PER_ATTACK = 0.5
+ATTACK_PER_TIME = 1.0 / TIME_PER_ATTACK
+FRAMES_PER_ATTACK = 6
 
 class Idle:
 
@@ -75,28 +75,61 @@ class Idle:
             self.player.image.clip_composite_draw(0, 35, 33, 22,
                                                   0, 'h', self.player.x, self.player.y, 75, 75)
 
+run_sprites = [
+    (0, 0), (32, 0), (64, 0), (96, 0),
+    (128, 0), (160, 0), (192, 0), (224, 0)
+]
+
 class Run:
-    def __init__(self, boy):
-        self.boy = boy
+    def __init__(self, player):
+        self.player = player
 
     def enter(self, e):
-        if right_down(e) or left_up(e):
-            self.boy.dir = self.boy.face_dir = 1
-        elif left_down(e) or right_up(e):
-            self.boy.dir = self.boy.face_dir = -1
+        # 키 입력에 따라 방향 설정
+        if right_down(e):
+            self.player.xdir += 1
+        if left_down(e):
+            self.player.xdir -= 1
+        if up_down(e):
+            self.player.ydir += 1
+        if down_down(e):
+            self.player.ydir -= 1
+
+        # 바라보는 방향 설정
+        if self.player.xdir > 0:
+            self.player.face_dir = 1
+        elif self.player.xdir < 0:
+            self.player.face_dir = -1
 
     def exit(self, e):
-        pass
+        # 키를 뗄 때 해당 방향의 이동을 멈춤
+        if right_up(e):
+            self.player.xdir -= 1
+        if left_up(e):
+            self.player.xdir += 1
+        if up_up(e):
+            self.player.ydir -= 1
+        if down_up(e):
+            self.player.ydir += 1
+
+        if self.player.xdir > 0:
+            self.player.face_dir = 1
+        elif self.player.xdir < 0:
+            self.player.face_dir = -1
+
 
     def do(self):
-        self.boy.frame = (self.boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        self.boy.x += self.boy.dir * RUN_SPEED_PPS * game_framework.frame_time
+        self.player.frame = (self.player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        self.player.x += self.player.xdir * RUN_SPEED_PPS * game_framework.frame_time
+        self.player.y += self.player.ydir * RUN_SPEED_PPS * game_framework.frame_time
 
     def draw(self):
-        if self.boy.face_dir == 1: # right
-            self.boy.image.clip_draw(int(self.boy.frame) * 100, 100, 100, 100, self.boy.x, self.boy.y)
-        else: # face_dir == -1: # left
-            self.boy.image.clip_draw(int(self.boy.frame) * 100, 0, 100, 100, self.boy.x, self.boy.y)
+        if self.player.face_dir == 1:  # right
+            self.player.image.clip_composite_draw(run_sprites[int(self.player.frame)][0], run_sprites[int(self.player.frame)][1], 31, 22,
+                                                  0, ' ', self.player.x, self.player.y, 75, 75)
+        else:  # face_dir == -1: # left
+            self.player.image.clip_composite_draw(run_sprites[int(self.player.frame)][0], run_sprites[int(self.player.frame)][1], 31, 22,
+                                                  0, 'h', self.player.x, self.player.y, 75, 75)
 
 class Player:
     def __init__(self):
@@ -108,20 +141,25 @@ class Player:
         self.ydir = 0
         self.image = load_image('resources/sprites/sword_move.png')
         self.attack = load_image('resources/sprites/sword_attack.png')
-        # self.image.set_colorkey((47, 54, 65))
 
         self.IDLE = Idle(self)
         self.RUN = Run(self)
         self.state_machine = StateMachine(
             self.IDLE,
             {
-                self.IDLE : {right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN, up_down: self.RUN, down_down: self.RUN, up_up: self.RUN, down_up: self.RUN},
-                self.RUN : {right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE, up_down: self.IDLE, down_down: self.IDLE, up_up: self.IDLE, down_up: self.IDLE}
+                # 이동 키가 눌리면 RUN 상태로 진입
+                self.IDLE: {right_down: self.RUN, left_down: self.RUN, up_down: self.RUN, down_down: self.RUN},
+                # RUN 상태에서 키가 눌리거나 떼어져도 RUN 상태를 유지
+                self.RUN: {right_down: self.RUN, left_down: self.RUN, up_down: self.RUN, down_down: self.RUN,
+                           right_up: self.RUN, left_up: self.RUN, up_up: self.RUN, down_up: self.RUN}
             }
         )
 
     def update(self):
         self.state_machine.update()
+        # 모든 이동이 멈추면 IDLE 상태로 전환
+        if self.state_machine.cur_state == self.RUN and self.xdir == 0 and self.ydir == 0:
+            self.state_machine.cur_state = self.IDLE
 
     def handle_event(self, event):
         self.state_machine.handle_state_event(('INPUT', event))
