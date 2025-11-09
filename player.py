b30f5773bf59407cfe69af545e6aa36517ce9dbc
PreaseP@H <dong123541@gmail.com>
2025-11-10 05:37:04 +0900

    캐릭터 공격 구현

--- player.py
+++ player.py
@@ -1,14 +1,18 @@
 from pico2d import load_image, get_time, load_font, draw_rectangle
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN, SDL_MOUSEBUTTONDOWN
 
 import game_world
 import game_framework
 
 from state_machine import StateMachine
+from sword_effect import SwordEffect
 
 
 def space_down(e): # e is space down ?
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def mouse_click(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONDOWN
 
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
@@ -51,7 +55,7 @@ FRAMES_PER_ACTION = 8
 # Player Move Action Speed
 TIME_PER_ATTACK = 0.5
 ATTACK_PER_TIME = 1.0 / TIME_PER_ATTACK
-FRAMES_PER_ATTACK = 6
+FRAMES_PER_ATTACK = 7
 
 class Idle:
 
@@ -131,6 +135,56 @@ class Run:
             self.player.image.clip_composite_draw(run_sprites[int(self.player.frame)][0], run_sprites[int(self.player.frame)][1], 31, 22,
                                                   0, 'h', self.player.x, self.player.y, 75, 75)
 
+attack_sprites = [
+    (0, 0, 62), (63, 0, 62), (126, 0, 62), (189, 0, 62),
+    (252, 0, 62), (315, 0, 62), (378, 0, 62)
+]
+
+class Attack:
+    def __init__(self, player):
+        self.player = player
+
+    def enter(self, e):
+        self.player.frame = 0  # 공격 프레임 초기화
+        effect = SwordEffect(self.player.x + self.player.face_dir * 50, self.player.y, self.player.face_dir)
+        game_world.add_object(effect, 1)
+        if right_down(e):
+            self.player.xdir += 1
+        if left_down(e):
+            self.player.xdir -= 1
+        if up_down(e):
+            self.player.ydir += 1
+        if down_down(e):
+            self.player.ydir -= 1
+
+    def exit(self, e):
+        if right_up(e):
+            self.player.xdir -= 1
+        if left_up(e):
+            self.player.xdir += 1
+        if up_up(e):
+            self.player.ydir -= 1
+        if down_up(e):
+            self.player.ydir += 1
+
+    def do(self):
+        # 공격 애니메이션 프레임 업데이트
+        self.player.frame = (self.player.frame + FRAMES_PER_ATTACK * ATTACK_PER_TIME * game_framework.frame_time)
+        self.player.x += self.player.xdir * RUN_SPEED_PPS * game_framework.frame_time
+        self.player.y += self.player.ydir * RUN_SPEED_PPS * game_framework.frame_time
+        # 공격 애니메이션이 끝나면 상태 전환
+
+        if self.player.frame >= FRAMES_PER_ATTACK:
+            self.player.state_machine.cur_state = self.player.RUN
+
+    def draw(self):
+        if self.player.face_dir == 1:  # right
+            self.player.attack.clip_composite_draw(attack_sprites[int(self.player.frame)][0], attack_sprites[int(self.player.frame)][1],
+                                                   attack_sprites[int(self.player.frame)][2], 31, 0, ' ', self.player.x - 5, self.player.y + 15, 100, 100)
+        else:  # face_dir == -1: # left
+            self.player.attack.clip_composite_draw(attack_sprites[int(self.player.frame)][0], attack_sprites[int(self.player.frame)][1],
+                                                   attack_sprites[int(self.player.frame)][2], 31, 0, 'h', self.player.x - 5, self.player.y + 15, 100, 100)
+
 class Player:
     def __init__(self):
 
@@ -144,14 +198,18 @@ class Player:
 
         self.IDLE = Idle(self)
         self.RUN = Run(self)
+        self.ATTACK = Attack(self)  # Attack 상태 인스턴스 생성
         self.state_machine = StateMachine(
             self.IDLE,
             {
                 # 이동 키가 눌리면 RUN 상태로 진입
-                self.IDLE: {right_down: self.RUN, left_down: self.RUN, up_down: self.RUN, down_down: self.RUN},
+                self.IDLE: {right_down: self.RUN, left_down: self.RUN, up_down: self.RUN, down_down: self.RUN,
+                            mouse_click: self.ATTACK},
                 # RUN 상태에서 키가 눌리거나 떼어져도 RUN 상태를 유지
                 self.RUN: {right_down: self.RUN, left_down: self.RUN, up_down: self.RUN, down_down: self.RUN,
-                           right_up: self.RUN, left_up: self.RUN, up_up: self.RUN, down_up: self.RUN}
+                           right_up: self.RUN, left_up: self.RUN, up_up: self.RUN, down_up: self.RUN,
+                           mouse_click: self.ATTACK},
+                self.ATTACK: {}
             }
         )
 
