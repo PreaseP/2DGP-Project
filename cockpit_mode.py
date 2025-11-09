d35f5df0ce80d19faf11a94cbe4c943937e314c5
PreaseP@H <dong123541@gmail.com>
2025-11-10 01:42:49 +0900

    타이틀에서 SPACE 키 입력시 조종부로 이동.

--- cockpit_mode.py
+++ cockpit_mode.py
@@ -1,6 +1,6 @@
 from pico2d import *
 import game_framework
-import cockpit_mode
+import play_mode
 
 def handle_events():
     event_list = get_events()
@@ -9,12 +9,12 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            game_framework.change_mode(cockpit_mode)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_a:
+            game_framework.change_mode(play_mode)
 
 def init():
     global image, font
-    image = load_image('resources/background/title.png')
+    image = load_image('resources/background/cockpit.png')
     font = load_font('resources/DungGeunMo.TTF', 50)
 
 def update():
@@ -23,7 +23,7 @@ def update():
 def draw():
     clear_canvas()
     image.draw(640, 360, 1280, 720)
-    font.draw(350, 100, 'Press SPACE to START', (255, 255, 255))
+    # font.draw(400, 100, 'Press A to farming', (255, 255, 255))
     update_canvas()
 
 
