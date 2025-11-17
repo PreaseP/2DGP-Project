f9dbcdc7bc0e6ee17e8a8c779a1b6b218c6ce7b4
L@PresaeP <dong123541@gmail.com>
2025-11-17 14:34:40 +0900

    Button class 추가 플레이어 무기를 총, 검으로 스왑할 수 있는 버튼 추가

--- cockpit_mode.py
+++ cockpit_mode.py
@@ -6,7 +6,23 @@ import lab_mode
 import armory_mode
 import userdata
 
+buttonList = []
+
+class Button:
+    def __init__(self, left, bottom, right, top, button_type):
+        self.left = left
+        self.bottom = bottom
+        self.right = right
+        self.top = top
+        self.button_type = button_type
+
 # 버튼 위치/크기 (화면 중앙)
+farmingButton = Button(640 - 175, 160 - 40, 640 + 175, 160 + 40, 'farming')  # left, bottom, right, top, button_type
+armorChoiceButton = Button(45 - 35, 45 - 35, 45 + 35, 45 + 35, 'armor_choice')
+
+buttonList.append(farmingButton)
+buttonList.append(armorChoiceButton)
+
 FARMING_CENTER_X = 640
 FARMING_CENTER_Y = 160
 FARMING_W = 350
@@ -27,17 +43,22 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             mx = event.x
             my = 720 - event.y  # pico2d는 원점이 아래이므로 y를 뒤집음
-            left = FARMING_CENTER_X - FARMING_W // 2
-            right = FARMING_CENTER_X + FARMING_W // 2
-            bottom = FARMING_CENTER_Y - FARMING_H // 2
-            top = FARMING_CENTER_Y + FARMING_H // 2
-            if left <= mx <= right and bottom <= my <= top:
-                game_framework.change_mode(farming_stage)
+            for button in buttonList:
+                if button.left <= mx <= button.right and button.bottom <= my <= button.top:
+                    if button.button_type == 'farming':
+                        game_framework.change_mode(farming_stage)
+                    elif button.button_type == 'armor_choice':
+                        if userdata.playerType == 'S':
+                            userdata.playerType = 'G'
+                        else:
+                            userdata.playerType = 'S'
 
 def init():
-    global image, font
+    global image, font, sword, gun
     image = load_image('resources/background/cockpit.png')
     font = load_font('resources/DungGeunMo.TTF', 50)
+    sword = load_image('resources/sprites/sword_icon.png')
+    gun = load_image('resources/sprites/gun_icon.png')
 
 def update():
     pass
@@ -46,20 +67,18 @@ def draw():
     clear_canvas()
     image.draw(640, 360, 1280, 720)
 
-    # 버튼 그리기 (채워진 사각형 + 텍스트)
-    left = FARMING_CENTER_X - FARMING_W // 2
-    right = FARMING_CENTER_X + FARMING_W // 2
-    bottom = FARMING_CENTER_Y - FARMING_H // 2
-    top = FARMING_CENTER_Y + FARMING_H // 2
-
     # 채워진 사각형: draw_rectangle을 여러 번 그려 채움 효과를 냄
     # 버튼 영역의 각 y에 대해 1픽셀 높이의 가로선을 그림
-    y = bottom
-    while y <= top:
-        draw_rectangle(left, y, right, y+1, 255, 255, 255 ,0)
+    y = buttonList[0].bottom
+    while y <= buttonList[0].top:
+        draw_rectangle(buttonList[0].left, y, buttonList[0].right, y+1, 255, 255, 255 ,0)
         y += 1
 
     font.draw(FARMING_CENTER_X - 160, FARMING_CENTER_Y, 'Farming Stage', (0, 0, 0))
+    if userdata.playerType == 'S':
+        sword.clip_draw(0, 0, 24, 24, 45, 45, 75, 75)
+    else:
+        gun.clip_draw(0, 0, 24, 24, 45, 45, 75, 75)
 
     update_canvas()
 
