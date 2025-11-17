11fba77df4a3eeae70f177ebfb7b6e2d2ead9091
L@PresaeP <dong123541@gmail.com>
2025-11-17 20:07:50 +0900

    버튼 draw 함수 추가 (테두리만 or 채워서)

--- button.py
+++ button.py
@@ -1,3 +1,5 @@
+from pico2d import draw_rectangle
+
 class Button:
     def __init__(self, left, bottom, right, top, button_type):
         self.left = left
@@ -5,3 +7,14 @@ class Button:
         self.right = right
         self.top = top
         self.button_type = button_type
+
+    def draw(self, r = 255, g = 255, b = 255, a = 255):
+        draw_rectangle(self.left, self.bottom, self.right, self.top, r, g, b, a)
+
+    def fill_draw(self, r = 255, g = 255, b = 255, a = 255):
+        # 채워진 사각형: draw_rectangle을 여러 번 그려 채움 효과를 냄
+        # 버튼 영역의 각 y에 대해 1픽셀 높이의 가로선을 그림
+        y = self.bottom
+        while y <= self.top:
+            draw_rectangle(self.left, y, self.right, y + 1, r, g, b, a)
+            y += 1
