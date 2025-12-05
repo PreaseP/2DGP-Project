fc8c55cd5d944f2915f14956867d03da91aa7a8b
PreaseP@H <dong123541@gmail.com>
2025-12-06 01:19:51 +0900

    스테이지 1 추가

---
+++ stage1.py
@@ -0,0 +1,64 @@
+import random
+from pico2d import *
+
+import cockpit_mode
+from general_stage import GeneralStage
+from players import PlayerS
+from playerg import PlayerG
+import game_framework
+import game_world
+import userdata
+
+import common
+
+spawn_timer = 0.0
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_mode(cockpit_mode)
+        else:
+            common.player.handle_event(event)
+
+def init():
+    common.map = GeneralStage('stage1')
+    game_world.add_object(common.map, 0)
+
+    common.map.right_border = 2
+    common.map.left_border = 0
+
+    if userdata.playerType == 'S':
+        common.player = PlayerS()
+    else:
+        common.player = PlayerG()
+
+    common.player.x = get_canvas_width() / 2
+    common.player.y = get_canvas_height() / 2
+
+    game_world.add_object(common.player, 1)
+    game_world.add_collision_pair('player:monster', common.player, None)
+
+def update():
+
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    userdata.show_status(common.player.hp)
+    update_canvas()
+
+def finish():
+    for layer in game_world.world:
+        for o in layer:
+            # collision_pairs에서 해당 오브젝트 제거
+            game_world.remove_collision_object(o)
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
