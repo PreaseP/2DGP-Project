4e88ec2ace4a9ba363cb241f04c5cb79169a10e2
PreaseP@H <dong123541@gmail.com>
2025-12-07 02:16:52 +0900

    보스 스테이지 추가

---
+++ boss_map.py
@@ -0,0 +1,26 @@
+from pico2d import load_image, get_canvas_width, get_canvas_height, clamp
+import common
+
+class BossMap:
+    def __init__(self):
+        self.image = load_image('resources/background/boss_stage.png')
+        self.cw = get_canvas_width()
+        self.ch = get_canvas_height()
+        self.w = self.image.w
+        self.h = self.image.h
+
+    def update(self):
+        self.window_left = clamp(0, int(common.player.x) - self.cw // 2, self.w - self.cw - 1)
+        self.window_bottom = clamp(0, int(common.player.y) - self.ch // 2, self.h - self.ch - 1)
+
+        common.player.x = clamp(50, common.player.x, common.map.w - 50)
+        common.player.y = clamp(50, common.player.y, common.map.h - 50)
+
+    def draw(self):
+        self.image.clip_draw_to_origin(
+            self.window_left, self.window_bottom, self.cw, self.ch,
+            0, 0 # 캔버스의 왼쪽 아래 코너에 그리기
+        )
+
+    def handle_event(self, event):
+        pass
---
+++ boss_stage.py
@@ -0,0 +1,74 @@
+import random
+from pico2d import *
+
+import cockpit_mode
+import death_mode
+import pause_menu
+from boss_map import BossMap
+from players import PlayerS
+from playerg import PlayerG
+import game_framework
+import game_world
+import userdata
+
+import common
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.push_mode(pause_menu)
+        else:
+            common.player.handle_event(event)
+
+def init():
+    global small_font
+    common.map = BossMap()
+    game_world.add_object(common.map, 0)
+
+    small_font = load_font('resources/DungGeunMo.TTF', 20)
+
+    if userdata.playerType == 'S':
+        common.player = PlayerS()
+    else:
+        common.player = PlayerG()
+    game_world.add_object(common.player, 1)
+    game_world.add_collision_pair('player:monster', common.player, None)
+
+def update():
+
+    if common.player.hp == 0:
+        game_framework.push_mode(death_mode)
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    userdata.show_status(common.player.hp)
+
+    if userdata.playerType == 'S':
+        if common.player.dash_time > 0.0:
+            small_font.draw(480, 80, f'이동 스킬 재사용 대기시간 : {common.player.dash_time:.1f}초', (0, 255, 0))
+    else:
+        if common.player.slide_time > 0.0:
+            small_font.draw(480, 80, f'이동 스킬 재사용 대기시간 : {common.player.slide_time:.1f}초', (0, 255, 0))
+
+    if common.player.weapon_time > 0.0:
+        small_font.draw(480, 100, f'무기 스킬 대기시간 : {common.player.weapon_time:.1f}초', (255, 0, 0))
+
+    update_canvas()
+
+
+def finish():
+    for layer in game_world.world:
+        for o in layer:
+            # collision_pairs에서 해당 오브젝트 제거
+            game_world.remove_collision_object(o)
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
